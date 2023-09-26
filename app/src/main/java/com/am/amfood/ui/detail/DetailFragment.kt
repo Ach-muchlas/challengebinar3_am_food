36e278cf36e3873a54e3468772fe53c541696766
ahmad <ahmad.mukhlas61@gmail.com>
2023-09-26 23:08:23 +0700

    change code on the home and details views

--- app/src/main/java/com/am/amfood/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/am/amfood/ui/detail/DetailFragment.kt
@@ -10,12 +10,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.am.amfood.R
+import com.am.amfood.databinding.ContentDetailFragmentBinding
 import com.am.amfood.databinding.FragmentDetailBinding
 import com.am.amfood.model.CardModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
+    private lateinit var bindingContent: ContentDetailFragmentBinding
     private val binding get() = _binding!!
     private val args: DetailFragmentArgs by navArgs()
 
@@ -25,19 +27,27 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        bindingContent = binding.layoutContentDetail
+
+
         val bottom = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottom?.visibility = View.GONE
 
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
         val cardModel: CardModel = args.objectParcelable
 
+        /*mengisi value widget untuk tampilan yang ada di parent layout*/
         binding.apply {
             imageProduct.setImageResource(cardModel.imageProduct)
+            cardBack.setOnClickListener {
+                it.findNavController().navigate(R.id.action_detailFragment_to_navigation_home)
+            }
+        }
+
+        /*mengisi value dari beberapa widget di tampilan include*/
+        bindingContent.apply {
             textViewNameItem.text = cardModel.name
             textViewValueDesc.text = cardModel.desc
             textViewValuePrice.text = cardModel.price
@@ -50,11 +60,8 @@ class DetailFragment : Fragment() {
                 mapIntent.setPackage("com.google.android.apps.maps")
                 startActivity(mapIntent)
             }
-
-            cardBack.setOnClickListener {
-                it.findNavController().navigate(R.id.action_detailFragment_to_navigation_home)
-            }
         }
+        return view
     }
 
     override fun onDestroy() {
